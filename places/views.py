7545efc76aaf23e4964d931016d7ecf7c6e576a3
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2022-11-23 14:51:49 +0100

    HttpResponse

--- places/views.py
+++ places/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Place
+from django.http import Http404, HttpResponse
 
-def show_card(request):
+
+def show_places(request):
     features = []
     for place in Place.objects.all():
         feature = {
@@ -22,3 +24,12 @@ def show_card(request):
         "features": features
     }
     return render(request, 'index.html', context={"places":places})
+
+def place_detail_info(request, id):
+    try:
+        obj = Place.objects.filter(id=id).first()
+        print(obj)
+    except Place.DoesNotExist:
+        raise Http404("No MyModel matches the given query.")
+    finally:
+        return HttpResponse(obj.title)
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from places.views import show_card
+from places.views import show_places, place_detail_info
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', show_card),
+    path('places', show_places),
+    path('places/<id>', place_detail_info),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
