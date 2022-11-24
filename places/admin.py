f2ae0e024e3cd09d6fc8a30c8d63f2d00f860203
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2022-11-24 13:29:42 +0100

    added inlines to admin

--- places/admin.py
+++ places/admin.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from .models import Place, ImagePlace
 
 
+class ImagePlaceInline(admin.TabularInline):
+    model = ImagePlace
+    exclude = ()
+    extra = 0
+    fields = ('image', 'position',)
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('title', 'description_short',)
-
-
-@admin.register(ImagePlace)
-class ImagePlaceAdmin(admin.ModelAdmin):
-    list_display = ('title', 'place')
-    ordering = ('-id', 'place')
+    inlines = (ImagePlaceInline,)
--- places/models.py
+++ places/models.py
@@ -19,6 +19,7 @@ class Place(models.Model):
 class ImagePlace(models.Model):
     title = models.CharField(max_length=256)
     image = models.ImageField()
+    position = models.PositiveIntegerField(default=0)
     place = models.ForeignKey(Place, related_name='imgs', on_delete=models.CASCADE)
 
     def __str__(self):
