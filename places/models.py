f1679707b3029892a85cb4ec0785e657f528ef6d
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2022-11-24 17:27:56 +0100

    added tinymce

--- places/models.py
+++ places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField(max_length=256)
     description_short = models.TextField(blank=True, null=True)
-    description_long = models.TextField(blank=True, null=True)
+    description_long = HTMLField()
     coordinate_lng = models.FloatField(blank=True, null=True)
     coordinate_lat = models.FloatField(blank=True, null=True)
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -4,8 +4,11 @@ from places.views import show_places, place_detail_info
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import include
 
 urlpatterns = [
+    path('tinymce/', include('tinymce.urls')),
+
     path('admin/', admin.site.urls),
     path('places', show_places, name='places_list'),
     path('places/<id>', place_detail_info, name='place_detail_info'),
