54c44f6b2ff7854bafa3d64a301188064355aaf4
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2022-11-29 18:50:52 +0100

    fixed null and blank

--- places/models.py
+++ places/models.py
@@ -4,14 +4,13 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=256)
-    description_short = models.TextField(null=True)
-    description_long = HTMLField(null=True)
-    coordinate_lng = models.FloatField(null=True)
-    coordinate_lat = models.FloatField(null=True)
+    description_short = models.TextField(blank=True, null=True)
+    description_long = HTMLField(blank=True, null=True)
+    coordinate_lng = models.FloatField(default=0)
+    coordinate_lat = models.FloatField(default=0)
 
     def __str__(self):
         return self.title
-
 
     class Meta:
         verbose_name_plural = "Локации"
@@ -21,7 +20,6 @@ class ImagePlace(models.Model):
     image = models.ImageField()
     position = models.PositiveIntegerField(default=0)
     place = models.ForeignKey(Place, related_name='imgs', on_delete=models.CASCADE)
-
 
     class Meta:
         verbose_name_plural = "Картинки"
