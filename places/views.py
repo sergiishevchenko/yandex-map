d0c9520fbd040f369db2b1913294d0cdd7ff25ca
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2022-12-03 11:42:56 +0100

    quotes&get_object_or_404

--- places/views.py
+++ places/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from django.urls import reverse
 
@@ -9,38 +9,38 @@ def show_places(request):
     features = []
     for place in Place.objects.all():
         feature = {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [place.lng, place.lat]
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.lng, place.lat]
             },
-            "properties": {
-                "title": place.title,
-                "placeId": place.id,
-                "detailsUrl": reverse('place_detail_info', args=[place.id])
+            'properties': {
+                'title': place.title,
+                'placeId': place.id,
+                'detailsUrl': reverse('place_detail_info', args=[place.id])
             }
         }
         features.append(feature)
+
     places = {
-        "type": "FeatureCollection",
-        "features": features
+        'type': 'FeatureCollection',
+        'features': features
     }
-    return render(request, 'index.html', context={"places": places})
+    return render(request, 'index.html', context={'places': places})
 
 
-def place_detail_info(request, id):
-    place = Place.objects.filter(id=id).first()
-    update_imgs = []
-    for img in list(place.imgs.all()):
-        update_imgs.append(img.image.url)
+def place_detail_info(request, pk):
+    place = get_object_or_404(Place, pk=pk)
+    update_imgs = [img.image.url for img in place.imgs.all()]
+
     json_response = {
-        "title": place.title,
-        "imgs": update_imgs,
-        "description_short": place.description_short,
-        "description_long": place.description_long,
-        "coordinates": {
-            "lat": place.lat,
-            "lng": place.lng
+        'title': place.title,
+        'imgs': update_imgs,
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lat': place.lat,
+            'lng': place.lng
         }
     }
     return JsonResponse(json_response, json_dumps_params={'indent': 4, 'ensure_ascii': False})
