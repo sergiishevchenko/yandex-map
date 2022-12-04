88556ef067ac508ecd0620ba74a270aeb4f151a5
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2022-12-04 21:47:17 +0100

    str(e) -> e & flake-8

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -18,14 +18,17 @@ class Command(BaseCommand):
         try:
             response.raise_for_status()
         except Exception as e:
-            return 'Ошибка при загрузке новой локации: {}'.format(str(e))
+            return 'Ошибка при загрузке новой локации: {}'.format(e)
 
         raw_place = response.json()
         place, created = Place.objects.get_or_create(
             title=raw_place['title'],
             lng=raw_place['coordinates']['lng'],
             lat=raw_place['coordinates']['lat'],
-            defaults={'description_short': raw_place.get('description_short', ''), 'description_long': raw_place.get('description_long', '')},
+            defaults={
+                'description_short': raw_place.get('description_short', ''),
+                'description_long': raw_place.get('description_long', '')
+            },
         )
         if created:
             for img in raw_place.get('imgs', []):
@@ -33,6 +36,6 @@ class Command(BaseCommand):
                 try:
                     image_response.raise_for_status()
                 except Exception as e:
-                    return 'Ошибка при загрузке новой фотографии: {}'.format(str(e))
+                    return 'Ошибка при загрузке новой фотографии: {}'.format(e)
                 content_file = ContentFile(image_response.content, '{}'.format(uuid.uuid4()))
                 ImagePlace.objects.create(place=place, image=content_file)
